80684ade07c98da7ca16464a803c72aab50d9ab0
Marcel Marnitz <marcelmarnitz@googlemail.com>
2019-06-23 19:48:51 +0200

    Remove jquery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,34 @@
 require('../css/app.scss');
 
-var $ = require('jquery');
-require('bootstrap');
-var Clipboard = require('clipboard');
+require('bootstrap.native');
+let Clipboard = require('clipboard');
 
-// Make jQuery available
-window.$ = $;
+/*
+ * Polyfill for closest function (thanks, Mozilla! https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill)
+ */
+if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.msMatchesSelector ||
+        Element.prototype.webkitMatchesSelector;
+}
 
-+function($) {
-    'use strict';
+if (!Element.prototype.closest) {
+    Element.prototype.closest = function(s) {
+        let el = this;
 
-    $(document).ready(function() {
-        $('[data-trigger="submit"]').change(function(e) {
-            var $this = $(this);
-            $this.closest('form').submit();
+        do {
+            if (el.matches(s)) return el;
+            el = el.parentElement || el.parentNode;
+        } while (el !== null && el.nodeType === 1);
+        return null;
+    };
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('[data-trigger="submit"]').forEach(function (el) {
+        el.addEventListener('change', function (event) {
+            this.closest('form').submit();
         });
-
-        new Clipboard('button[data-clipboard-target]');
     });
-}(jQuery);
+
+    new Clipboard('button[data-clipboard-target]');
+});
